60fbf85d6115fd26bcab573671d3fb1bf8d6f994
oleghbond <o.o.bondarenko@gmail.com>
2025-03-24 12:05:33 +0200

    update

--- gs2note.py
+++ gs2note.py
@@ -13,17 +13,25 @@ table = 'persons'
 # Встановлюємо повні шляхи до фолдерів коду і даних
 code_dir, data_dir = ou.set_dirs(folders)
 
-# Зчитуємо вхідні дані з таблиці Гугл Форми і опис конвертування до Обсідіан
+# Зчитуємо вхідні дані з таблиці Гугл Форми і опис її конвертування до Обсідіан
 dfgf, TBL_STRUCT = ou.read_check_gs_table(table, code_dir)
+
+# Перетворення значень стовпчиків зі списку `TBL_STRUCT['pers_name']` з 
+# вхідного датафрейму: "Прізвище Ім'я По-батькові" -> "Ім'я Прізвище"
+dfgf = ou.pib2ip(dfgf, TBL_STRUCT)
 
 import pandas as pd
 
-dfob_cols = {'title': TBL_STRUCT['title']} | TBL_STRUCT['label_refs'] 
+# Пряма і зворотня мапи (мітки нотаток Обсідіан) <--> (назви стовпчиків таблиці Гугл Форми)
+dfob_cols = TBL_STRUCT['label_refs']
 dfob_cols_i = {v: k for k, v in dfob_cols.items()}
 
 dfob = dfgf.copy()
 dfob.rename(columns=dfob_cols_i, inplace=True)
+if 'title' not in dfob.columns:
+    dfob['title'] = dfgf[dfob_cols['title']]
 dfob['Позначка часу'] = pd.to_datetime(dfob['Позначка часу'])
+
 
 
 #dfob_cols |= TBL_STRUCT['label_vals']
--- obsidian_util.py
+++ obsidian_util.py
@@ -38,7 +38,7 @@ def get_gs_table_url(conf: dict, table: str) -> str:
     словника `conf` '''
     tables = list(conf['tables'])
     if table not in tables:
-        sys.exit(f'Таблиця `{table}` відсутня серед таблиць {tables}')
+        sys.exit(f'🚩 Таблиця `{table}` відсутня серед таблиць {tables}')
     return conf['gs_domain'] + conf['tables'][table]['gs_id'] + '/edit#gid=0'
 
 def read_gs_by_url(table_url: str, 
@@ -62,37 +62,57 @@ def read_gs_by_url(table_url: str,
     
     return df
 
-def check_table_struct(conf: dict, table: str, df: pd.DataFrame) -> (dict, dict):
-    ''' Мінімальна валідація відповідності структури таблиці і конфігураційного 
-    словника '''
-    def clean_str(lst):
+''' KEY_LISTS - Функціональні списки назв стовпчиків вх. таблиці Гугл Форми:
+  - cols - всі стовпчики, 
+  - frontmatter - мітки фронтматерії нотатки Обсідіан, 
+  - sections - заголовки розділів нотатки Обсідіан, 
+  - not_required - необов'язкові поля Гугл Форми', 
+  - pers_name - стовпчики, що потребують перетворення: П.І.Б. -> І.П., 
+  - linked - стовпчики, що потребують перетворення: value -> [[value]]
+'''
+KEY_LISTS = ['cols', 'frontmatter', 'sections', 'not_required', 'pers_name', 'linked']
+
+def check_table_struct(df: pd.DataFrame, TBL_STRUCT: dict) -> list[str]:
+    ''' Мінімальна валідація структури вх. таблиці Гугл Форми і словника з 
+    описом конвертування цієї таблиці до Обсідіан.
+    Вхідні аргументи:
+        df - датафрейм вх. таблиці Гугл Форми;
+        TBL_STRUCT - описом конвертування вх. таблиці Гугл Форми до Обсідіан.
+    Повертає:
+        problems - список ключів словника `cond`, де виникли невідповідності 
+        (False) ключових елементів `TBL_STRUCT`, `cols`, `df.columns`, 
+        `tbl_struct_keys`; якщо все гаразд, то список порожній.
+    '''
+    def clean_str(lst: str | list[str]) -> str | list[str]:
+        ''' Прибирання зайвих пробілів у рядку по краях і всередині  '''
         if isinstance(lst, str):
             lst = ' '.join(lst.split())
         elif isinstance(lst, list):
-            lst = [' '.join(s.split()) for s in lst] if len(lst) > 0 else []
+            lst = [' '.join(s.split()) for s in lst]
         else:
-            sys.exit(f'Або str або list[str]: {lst}')
+            sys.exit(f'🚩 Або str або list[str]: {lst}')
         return lst
+    # Перевіряє чи є `lst` підмножиною `cols`, якщо `lst` порожнє, то повертає True
     check_cols = lambda lst: True if len(lst) == 0 else set(lst).issubset(cols)
 
-    # Прибираємо зайві пробіли у заголовках вхідної таблиці
+    # Прибираємо зайві пробіли у заголовках вх. датафрейму df
     c = clean_str(list(df.columns))
     c_map = {a:b for (a,b) in zip(df.columns, c)}
     df.rename(columns=c_map, inplace=True)
     
-    TBL_STRUCT = conf['tables'][table]
-    
-    lsts = ['cols', 'title', 'frontmatter', 'sections', 'not_required', 
-            'pers_name', 'linked']
-    for lst in lsts:
+    # Прибираємо зайві пробіли у елементах списків `KEY_LISTS` з `TBL_STRUCT`
+    for lst in KEY_LISTS:
         TBL_STRUCT[lst] = clean_str(TBL_STRUCT[lst])
     for k, v in TBL_STRUCT['label_refs'].items():
         TBL_STRUCT['label_refs'][k] = clean_str(v)
 
+    # список всіх заголовків вх. таблиці
     cols = set(TBL_STRUCT['cols'])
-    tbl_struct_keys = ['subdir', 'name', 'cols', 'title', 'frontmatter', 
-                       'sections', 'not_required', 'pers_name', 'linked', 
-                       'label_refs', 'label_vals', 'gs_id']
+    # Всі ключі словника `TBL_STRUCT`
+    tbl_struct_keys = ['subdir', 'name', 'cols', 'frontmatter', 'sections', 
+                       'not_required', 'pers_name', 'linked', 'label_refs', 
+                       'label_vals', 'gs_id']
+    # Валідація всіх списків
     cond = {
         'tbl_struct_keys': set(TBL_STRUCT.keys()) == set(tbl_struct_keys),
         'df_columns':   cols.issubset(set(df.columns)),
@@ -102,10 +122,10 @@ def check_table_struct(conf: dict, table: str, df: pd.DataFrame) -> (dict, dict)
         'not_required': check_cols(TBL_STRUCT['not_required']),
         'pers_name':    check_cols(TBL_STRUCT['pers_name']),
         'sections':     check_cols(TBL_STRUCT['sections']),
-        'title':        check_cols([TBL_STRUCT['title']]),
         }
+    # Список ключів словника `cond`, де виникли невідповідності (False)
     problems = [k for k, v in cond.items() if not v]
-    return problems, TBL_STRUCT
+    return problems
 
 def read_check_gs_table(table: str, code_dir: str) -> (pd.DataFrame, dict):
     ''' Зчитує вхідні дані з таблиці Гугл Форми `table` і опис конвертування до 
@@ -126,17 +146,36 @@ def read_check_gs_table(table: str, code_dir: str) -> (pd.DataFrame, dict):
     df = read_gs_by_url(table_url, cred_file)
     
     if len(df) == 0:
-        sys.exit(f'Таблиця `{table}` порожня!')
+        sys.exit(f'🚩 Таблиця `{table}` порожня!')
     else:
-        print(f'Розмір таблиці `{table}` - {df.shape}')
+        print(f'✅ Розмір таблиці `{table}` - {df.shape}')
     
-    # Мінімальна валідація відповідності структури таблиці і конфігураційного словника
-    problems, TBL_STRUCT = check_table_struct(conf, table, df)
+    # Опис конвертування вх. таблиці Гугл Форми `table` до Обсідіан
+    TBL_STRUCT = conf['tables'][table] # 
     
+    # Мінімальна валідація структури таблиці і словника опису конвертування
+    problems = check_table_struct(df, TBL_STRUCT)
+    
+    # Якщо все гаразд, то список `problems` порожній
     if problems:
-        sys.exit(f'Структурні проблеми в таблиці `{table}` такі: {problems}')
+        sys.exit(f'🚩 Структурні проблеми в таблиці `{table}` такі: {problems}')
     else:
-        print(f'Валідація відповідності структури таблиці `{table}` пройдена')
+        print(f'✅ Валідація відповідності структури таблиці `{table}` пройдена')
 
     return df, TBL_STRUCT
+
+def pib2ip(dfgf: pd.DataFrame, TBL_STRUCT: dict) -> pd.DataFrame:
+    ''' Перетворення значень стовпчиків зі списку `TBL_STRUCT['pers_name']` з 
+    вхідного датафрейму: "Прізвище Ім'я По-батькові" -> "Ім'я Прізвище"
+    '''
+    for pn in TBL_STRUCT['pers_name']:
+        pns = dfgf[pn]
+        names = []
+        for i, v in pns.items():
+            vl = v.split()
+            if len(vl) != 3:
+                sys.exit(f'🚩 Запис має містити 3 елементи - П.І.Б.: {v}')
+            names.append(' '.join([vl[1], vl[0]]))
+        dfgf[pn] = names
+    return dfgf
 
