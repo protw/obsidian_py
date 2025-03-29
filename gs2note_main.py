5fc4fa29cefb3e976d83e4f31eb69054d9fa1693
oleghbond <o.o.bondarenko@gmail.com>
2025-03-29 11:16:47 +0200

    update

--- gs2note_main.py
+++ gs2note_main.py
@@ -4,39 +4,20 @@ from gs2note import GS2ON_Convertor
 
 IN_COLAB = 'google.colab' in sys.modules
 
-def run(vault: str='NECU', table: str='persons', 
-        folders: dict={}, cond: dict={}):
+def run(vault: str, table: str, folders: dict, cond: dict) -> GS2ON_Convertor:
     if IN_COLAB:
-        if not folders:
-            folders = {
-                'base': '/content/drive',
-                'vaults_base': '/MyDrive/OBSIDIAN/',
-                'code_dir': 'obsidian-py/',
-                'vault_dir': vault + '/'
-                }
         from google.colab import drive
         # монтування Гугл Диску
         drive.mount(folders['base'])
-    else:
-        if not folders:
-            folders = {
-                'base': 'D:/boa_uniteam/',
-                'vaults_base': 'OBSIDIAN/',
-                'code_dir': 'obsidian-py/',
-                'vault_dir': vault + '/'
-                }
-
-    if not cond:
-        cond = {
-            'is_dupl_in_vault': True, # Рекомендовано True, за замовчанням
-            'is_dupl_in_table': True,  # Рекомендовано True, за замовчанням
-            ## ---- СХОВАТИ ВІД КОРИСТУВАЧА ----
-            'not_write_dupl':   True,  # Рекомендовано True, за замовчанням
-            'is_clean_writing': True  # За замовчанням True
-            }
 
     # Ініціюємо конвертор. Встановлюємо повні шляхи до фолдерів коду і даних
     conv = GS2ON_Convertor(folders, **cond)
+    
+    print('ВХІДНІ ПАРАМЕТРИ'
+          f'✅ Фолдер Обсідіан-сховища: {conv.vault_dir}\n'
+          f'✅ Фолдер налаштувань: {conv.code_dir}\n'
+          f'✅ Тип конвертованої таблиці: {table}'
+          'ЗАПУСК КОНВЕРТОРА')
     
     # Основоний метод класу `GS2ON_Convertor`
     conv.main(table)
@@ -44,17 +25,33 @@ def run(vault: str='NECU', table: str='persons',
     return conv
 
 if __name__ == '__main__':
+    # ==== ВХІДНІ ПАРАМЕТРИ ====
+
+    # Оберіть Обсідіан-сховище (vault):
+    vault = 'NECU' # обрати зі списку ['NECU', 'NCP-NEB']
+
+    # Оберіть тип таблиці (table), що буде конвертований з Гугл форми
+    # до обраного сховища:
+    table = 'persons' # обрати зі списку ['persons', 'teams', 'orgs', 'proj']
+
+    # ==== УВАГА! Цей розділ лише для адміна!! ====
+
+    # Якщо ви не знаєте призначення налаштувань нижче, або не впевнені в тому
+    # що знаєте, то нічого не чипайте
+    folders = {
+        'base': 'D:/boa_uniteam/',
+        'vaults_base': 'OBSIDIAN/',
+        'code_dir': 'obsidian-py/',
+        'vault_dir': vault + '/'
+    }
+    # В робочому положенні ВСІ елементи словника `cond` мають бут встановлені `True`
     cond = {
         'is_dupl_in_vault': False, # Рекомендовано True, за замовчанням
         'is_dupl_in_table': True,  # Рекомендовано True, за замовчанням
         ## ---- СХОВАТИ ВІД КОРИСТУВАЧА ----
         'not_write_dupl':   False,  # Рекомендовано True, за замовчанням
         'is_clean_writing': False  # За замовчанням True
-        }
-    # Визначаємо сховище
-    vault = 'NECU'
-    # Яку таблицю зчитуємо
-    table = 'persons'
-    
-    conv = run(vault=vault, table=table, cond=cond)
+    }
+
+    conv = run(vault=vault, table=table, folders=folders, cond=cond)
     
